ca1a7d8df6e289ff4a418bb9458b7fea0a427691
guoyi <15776547151@163.com>
2023-09-05 22:11:32 +0800

    第一次提交

---
+++ src/main/java/com/gyb/demo/MySpringCloudGatewayDemoApplication.java
@@ -0,0 +1,13 @@
+package com.gyb.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringCloudGatewayDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringCloudGatewayDemoApplication.class, args);
+    }
+
+}
